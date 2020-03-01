ae60e2c1eabcb3a107c059effb9d1198ce63c884
Peter van Haaften <petervh@gmail.com>
2020-03-01 17:01:19 -0500

    updated global host

---
+++ boot/timeline_service.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo ***waiting 2sec before timeline boot***
+sleep 2
+echo ***booting timeline supercollider global host***
+sclang -D /home/asterisk/src/timeline/TL_global_host.scd &
+emacs /home/asterisk/Documents/notes/timeline_linux_jan30_feb13update.org &
+catia

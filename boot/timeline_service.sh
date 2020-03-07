1f6d34275ff147470f358266b579259c93805fc2
Peter van Haaften <petervh@gmail.com>
2020-03-06 20:12:41 -0500

    pd osc script + related

--- boot/timeline_service.sh
+++ boot/timeline_service.sh
@@ -3,5 +3,7 @@ echo ***waiting 2sec before timeline boot***
 sleep 2
 echo ***booting timeline supercollider global host***
 sclang -D /home/asterisk/src/timeline/TL_global_host.scd &
-emacs /home/asterisk/Documents/notes/timeline_linux_jan30_feb13update.org &
+#emacs /home/asterisk/Documents/notes/timeline_linux_jan30_march3update.org &
 catia
+# T7#T7sleep 10
+#env -u SESSION_MANAGER -u DBUS_SESSION_BUS_ADDRESS vncserver -geometry 1600x1050

70b30c698b9ae5f8104cc135e1042f107b68c33b
Peter van Haaften <petervh@gmail.com>
2020-05-29 23:58:36 -0400

    nightime update

--- boot/timeline_service.sh
+++ boot/timeline_service.sh
@@ -4,6 +4,6 @@ sleep 2
 echo ***booting timeline supercollider global host***
 sclang -D /home/asterisk/src/timeline/TL_global_host.scd &
 #emacs /home/asterisk/Documents/notes/timeline_linux_jan30_march3update.org &
-catia
-# T7#T7sleep 10
-#env -u SESSION_MANAGER -u DBUS_SESSION_BUS_ADDRESS vncserver -geometry 1600x1050
+catia &
+emacs /home/asterisk/Documents/notes/timeline_linux_jan30_march6.org
+

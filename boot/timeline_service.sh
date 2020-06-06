7a971ff173bca90a07e8c094e0d3fd19c1e05189
Peter van Haaften <petervh@gmail.com>
2020-06-06 19:35:43 -0400

    fixing up for partyline

--- boot/timeline_service.sh
+++ boot/timeline_service.sh
@@ -5,5 +5,6 @@ echo ***booting timeline supercollider global host***
 sclang -D /home/asterisk/src/timeline/TL_global_host.scd &
 #emacs /home/asterisk/Documents/notes/timeline_linux_jan30_march3update.org &
 catia &
-emacs /home/asterisk/Documents/notes/timeline_linux_jan30_march6.org
-
+emacs /home/asterisk/Documents/notes/timeline_linux_jan30_june5.org &
+#xfce4-terminal -e, 'bash -c "asterisk -rvvvvv"' &
+xfce4-terminal -e, 'bash -c "htop"'

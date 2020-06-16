d8db9f42a4cfa0828e78a5adce375ded2187cce7
Peter van Haaften <petervh@gmail.com>
2020-06-15 20:40:36 -0400

    accounting for channel hangup

--- boot/timeline_service.sh
+++ boot/timeline_service.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 echo ***waiting 2sec before timeline boot***
-sleep 2
+sleep 4
 echo ***booting timeline supercollider global host***
-sclang -D /home/asterisk/src/timeline/TL_global_host.scd &
 #emacs /home/asterisk/Documents/notes/timeline_linux_jan30_march3update.org &
+cadence &
 catia &
 emacs /home/asterisk/Documents/notes/timeline_linux_jan30_june5.org &
-#xfce4-terminal -e, 'bash -c "asterisk -rvvvvv"' &
+# sclang -D /home/asterisk/src/timeline/TL_global_host.scd &
+scide /home/asterisk/src/timeline/TL_global_host.scd &
+xfce4-terminal -e, 'bash -c "asterisk -rvvvvv"' &
 xfce4-terminal -e, 'bash -c "htop"'

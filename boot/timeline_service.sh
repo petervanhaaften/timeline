38033121688b7f3c87daa6daebb524536069910b
Peter van Haaften <petervh@gmail.com>
2020-06-20 15:57:52 -0400

    repairing score/callflow

--- boot/timeline_service.sh
+++ boot/timeline_service.sh
@@ -5,8 +5,13 @@ echo ***booting timeline supercollider global host***
 #emacs /home/asterisk/Documents/notes/timeline_linux_jan30_march3update.org &
 cadence &
 catia &
+#firefox &
 emacs /home/asterisk/Documents/notes/timeline_linux_jan30_june5.org &
 # sclang -D /home/asterisk/src/timeline/TL_global_host.scd &
 scide /home/asterisk/src/timeline/TL_global_host.scd &
 xfce4-terminal -e, 'bash -c "asterisk -rvvvvv"' &
 xfce4-terminal -e, 'bash -c "htop"'
+#define which workspace applications load onto
+#xfce4-terminal -e, 'bash -c "wmctrl -r firefox -t 2"'
+#xfce4-terminal -e, 'bash -c "wmctrl -r scide -t 1"' &
+

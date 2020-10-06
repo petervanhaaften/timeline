5bb453cdad9cd085d7c5985c479b2f44ee7c1b8f
Peter van Haaften <petervh@gmail.com>
2020-10-05 20:18:59 -0400

    new from summer

--- boot/timeline_service.sh
+++ boot/timeline_service.sh
@@ -9,8 +9,8 @@ catia &
 emacs /home/asterisk/Documents/notes/timeline_linux_jan30_june5.org &
 # sclang -D /home/asterisk/src/timeline/TL_global_host.scd &
 scide /home/asterisk/src/timeline/TL_global_host.scd &
-xfce4-terminal -e, 'bash -c "asterisk -rvvvvv"' &
-xfce4-terminal -e, 'bash -c "htop"'
+xfce4-terminal -e, 'bash -c "asterisk -rvvvvv"'
+# xfce4-terminal -e, 'bash -c "htop"'
 #define which workspace applications load onto
 #xfce4-terminal -e, 'bash -c "wmctrl -r firefox -t 2"'
 #xfce4-terminal -e, 'bash -c "wmctrl -r scide -t 1"' &

95a9a250553e84d726656d9b2750d1efd123c4a2
Peter van Haaften <petervh@gmail.com>
2021-02-21 10:41:04 -0500

    fixing for multi-call

--- boot/timeline_service.sh
+++ boot/timeline_service.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-echo ***waiting 2sec before timeline boot***
-sleep 4
-echo ***booting timeline supercollider global host***
+#echo ***waiting 2sec before timeline boot***
+#sleep 4
+#echo ***booting timeline supercollider global host***
 #emacs /home/asterisk/Documents/notes/timeline_linux_jan30_march3update.org &
 cadence &
 catia &
 #firefox &
-emacs /home/asterisk/Documents/notes/timeline_linux_jan30_june5.org &
+#emacs /home/asterisk/Documents/notes/timeline_linux_jan30_june5.org &
 # sclang -D /home/asterisk/src/timeline/TL_global_host.scd &
-scide /home/asterisk/src/timeline/TL_global_host.scd &
-xfce4-terminal -e, 'bash -c "asterisk -rvvvvv"'
+#scide /home/asterisk/src/timeline/TL_global_host.scd &
+#xfce4-terminal -e, 'bash -c "asterisk -rvvvvv"'
 # xfce4-terminal -e, 'bash -c "htop"'
 #define which workspace applications load onto
 #xfce4-terminal -e, 'bash -c "wmctrl -r firefox -t 2"'

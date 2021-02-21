0141cd65c0fa175a9298cb22bfd2d3e58792feb2
Peter van Haaften <petervh@gmail.com>
2021-02-21 17:56:41 -0500

    final repair for multicall

--- startup_files/newbash2.sh
+++ startup_files/newbash2.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo | xfce4-terminal --tab --command "sclang /home/asterisk/src/timeline/startup_files/newstartup1.scd" --hold --tab --command "sclang /home/asterisk/src/timeline/startup_files/newstartup2.scd" --hold --tab --command "sclang /home/asterisk/src/timeline/startup_files/newstartup3.scd" --hold
+echo | xfce4-terminal --tab --command "sclang /home/asterisk/src/timeline/startup_files/newstartup1.scd -u 57121" --hold --tab --command "sclang /home/asterisk/src/timeline/startup_files/newstartup2.scd -u 57122" --hold --tab --command "sclang /home/asterisk/src/timeline/startup_files/newstartup3.scd -u 57123" --hold

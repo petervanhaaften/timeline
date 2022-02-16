f0c166c6f654f95d9a4c72c5f92a38a069b7bc87
Peter van Haaften <petervh@gmail.com>
2022-02-16 12:27:13 -0500

    boot scripts

--- boot/timeline_service.sh
+++ boot/timeline_service.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 #echo ***waiting 2sec before timeline boot***
-#sleep 4
 #echo ***booting timeline supercollider global host***
 #emacs /home/asterisk/Documents/notes/timeline_linux_jan30_march3update.org &
 cadence &
+sleep 5
 catia &
+sleep 5
+echo | xfce4-terminal --tab --command "sclang /home/asterisk/src/timeline/startup_files/newstartup1.scd -u 57121" --hold --tab --command "sclang /home/asterisk/src/timeline/startup_files/newstartup2.scd -u 57122" --hold --tab --command "sclang /home/asterisk/src/timeline/startup_files/newstartup3.scd -u 57123" --hold
+
 #firefox &
 #emacs /home/asterisk/Documents/notes/timeline_linux_jan30_june5.org &
 # sclang -D /home/asterisk/src/timeline/TL_global_host.scd &
